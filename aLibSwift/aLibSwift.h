5ed45c04cba5eacb18f284c6b2e89aa814e5592a
Haojun Jiang <kjiang22206@yahoo.com>
2019-08-18 17:01:29 -0400

    Initial Commit

---
+++ aLibSwift/aLibSwift.h
@@ -0,0 +1,19 @@
+//
+//  aLibSwift.h
+//  aLibSwift
+//
+//  Created by Haojun Jiang on 8/18/19.
+//  Copyright © 2019 Haojun Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for aLibSwift.
+FOUNDATION_EXPORT double aLibSwiftVersionNumber;
+
+//! Project version string for aLibSwift.
+FOUNDATION_EXPORT const unsigned char aLibSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <aLibSwift/PublicHeader.h>
+
+
